56a2e22d2091bcf0dd43034aaa8de93bb3078423
Bérenger AKODO <36599258+Berenger2@users.noreply.github.com>
2024-06-28 10:13:28 +0000

    ✨(mlops-api)mlflow interface config

--- run.py
+++ run.py
@@ -1,5 +1,17 @@
 from src.app import app
 import uvicorn
+from multiprocessing import Process
+from src.mlf import start_mlflow_ui
+
+def start_fastapi():
+    uvicorn.run(app, host="0.0.0.0", port=5030)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=5030)
+    p1 = Process(target=start_fastapi)
+    p2 = Process(target=start_mlflow_ui)
+
+    p1.start()
+    p2.start()
+
+    p1.join()
+    p2.join()
